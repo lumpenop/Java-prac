63388a4280f536f450a46565582608c06ed4ed0e
lumpenop <swk5373@gmail.com>
2025-05-04 06:10:35 +0900

    linked list

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,10 @@
 public class Main {
 
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
+    Node head = new Node("a");
+    head.next = new Node("b");
+    head.next.next = new Node("c");
 
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-
-
-    }
+    System.out.println(head.toString());
   }
 }
--- src/Node.java
+++ src/Node.java
@@ -2,4 +2,25 @@ public class Node {
 
   Object item;
   Node next;
+
+  public Node(Object item) {
+    this.item = item;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Node current = this;
+    sb.append("[");
+    while (current != null) {
+      sb.append(current.item);
+
+      if (current.next != null) {
+        sb.append("=>");
+      }
+      current = current.next;
+    }
+    sb.append("]");
+    return sb.toString();
+  }
 }
