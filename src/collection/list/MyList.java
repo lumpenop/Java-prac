d0dc768c99670aaa97e0eef2e3fee93eda4a19e2
lumpenop <swk5373@gmail.com>
2025-05-08 23:27:10 +0900

    Add: list interface

---
+++ src/collection/list/MyList.java
@@ -0,0 +1,19 @@
+package collection.list;
+
+public interface MyList<T> {
+
+  int size();
+
+  void add(T item);
+
+  void add(int index, T item);
+
+  T get(int index);
+
+  T set(int index, T item);
+
+  T remove(int index);
+
+  int indexOf(T item);
+  
+}
